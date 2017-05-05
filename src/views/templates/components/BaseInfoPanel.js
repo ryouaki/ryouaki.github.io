c89b15c7646dd18220dd5f28e3f4b6d527db0593
Ryou aki <46517115@qq.com>
2017-05-05 14:06:57 +0800

    update

--- src/views/templates/components/BaseInfoPanel.js
+++ src/views/templates/components/BaseInfoPanel.js
@@ -2,8 +2,11 @@ import React from 'react';
 
 class BaseInfoPanel extends React.Component {
     render() {
+        let { 
+            className
+        } = this.props;
         return(
-            <div>
+            <div className={className}>
                 <div className='baseinfo-panel-title'>
                     { this.props.title }
                 </div>
