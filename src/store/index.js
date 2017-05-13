d361c252d1de5320b7652700ebafb2423f7a63c9
Ryou aki <46517115@qq.com>
2017-05-13 22:18:17 +0800

    add some new middleware

--- src/store/index.js
+++ src/store/index.js
@@ -14,7 +14,6 @@ let reducers = initReducer();
 
 
 const createStoreWithMiddleware = applyMiddleware(logger)(createStore);
-// const store = createStoreWithMiddleware(reducer);
 
 export default () => {
     let store = createStoreWithMiddleware(
